2b0c4f266338c751c6e2775f69902d4e2c5fe16f
danfercf1 <danfercf@gmail.com>
2017-01-02 10:27:50 -0400

    Fix gcc 6.4

---
+++ services/install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+sudo apt-get install -y build-essential &&
+sudo apt-get install -y mysql-server &&
+sudo apt-get install -y libboost-all-dev &&
+sudo apt-get install -y libbz2-dev &&
+sudo apt-get install -y libmysqlclient-dev &&
+sudo apt-get install -y libgmp3-dev
+sudo apt-get install -y cmake
